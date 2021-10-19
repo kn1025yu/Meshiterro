d74273e775643f9646b1d2de14e35ac812fd0a07
kn1025yu <sn22kzh@gmail.com>
2021-10-19 07:30:27 +0000

    [start]コメント、いいね機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f8dc0e743d0d7c1ee2ddcf2cd10973b2b993bdd5d4b46a933c0a79db0fcad3526cb61776f774e56cfaa6addbced1224dd02d8d003cb8d706c0494ed1f3d14f43'
